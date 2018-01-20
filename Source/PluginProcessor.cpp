01f832ff2279ba654c1bdbca29f4577cd4bf407c
Wojtek <wojtekpilwinski@gmail.com>
2018-01-20 02:07:25 +0100

    renew master

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -11,7 +11,6 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-
 //==============================================================================
 WojtekSynthAudioProcessor::WojtekSynthAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
